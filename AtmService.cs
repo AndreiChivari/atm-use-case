57772a6eb49f84d94f037a4405dec20757d9341f
chivari.andrei <andrei.chivari@gmail.com>
2026-02-06 18:27:28 +0200

    First local commit

---
+++ AtmService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atm_use_case
+{
+    public class AtmService
+    {
+        public bool Autentificare(int pin)
+        {
+            return pin == 1234;
+        }
+
+        private void PrintareChitanta(double suma)
+        {
+            Console.WriteLine($"S-a eliberat chitanta pentru suma de {suma} lei!");
+        }
+
+        public void RetragereCash(int pin, double suma, bool chitanta) 
+        {
+
+            if (!(Autentificare(pin)))
+            {
+                return;
+            }
+
+            Console.WriteLine($"Ai retras suma de {suma} lei!");
+
+            if (chitanta)
+            {
+                PrintareChitanta(suma);
+            }
+
+
+
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atm_use_case
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AtmService op1 = new AtmService();
+            op1.RetragereCash(1234, 200, true);
+
+            Console.ReadKey();
+        }
+    }
+}
