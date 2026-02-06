c64fae3d3f0d378111fe4f087931b0fce78afd5f
chivari.andrei <andrei.chivari@gmail.com>
2026-02-06 18:31:27 +0200

    Updated version with Console.WriteLine() messages

--- AtmService.cs
+++ AtmService.cs
@@ -8,21 +8,28 @@ namespace atm_use_case
 {
     public class AtmService
     {
+        // USE CASE de tip INCLUDE (o includem ca apel de metoda in metoda RetragereCash())
+        // metoda RetragereCash() o apeleaza si se executa doar daca Autentificare returneaza True
         public bool Autentificare(int pin)
         {
+            Console.WriteLine("[INCLUDE] >> Verificare cod PIN ...");
             return pin == 1234;
         }
 
+        // USE CASE de tip EXTEND - optional (folosim IF pentru a o apela conditionat in RetragereCash)
         private void PrintareChitanta(double suma)
         {
-            Console.WriteLine($"S-a eliberat chitanta pentru suma de {suma} lei!");
+            Console.WriteLine($"[EXTEND] S-a eliberat chitanta pentru suma de {suma} lei!");
         }
 
+        // USE CASE PRINCIPAL
         public void RetragereCash(int pin, double suma, bool chitanta) 
         {
+            Console.WriteLine("Initializare operatiune...\n");
 
             if (!(Autentificare(pin)))
             {
+                Console.WriteLine("PIN incorect!");
                 return;
             }
 
